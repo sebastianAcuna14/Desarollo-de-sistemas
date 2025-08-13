b0a0027c6edf6c1d6f8f0bc9049108a40d4a1ba0
JoshuaMeji <156728872+JoshuaMeji@users.noreply.github.com>
2025-08-13 17:26:34 -0600

    Arreglo Datatable Finanza y Ventas

--- prototipo2/prototipo2/obj/Debug/net8.0/prototipo2.AssemblyInfo.cs
+++ prototipo2/prototipo2/obj/Debug/net8.0/prototipo2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prototipo2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99017cc851eb1405540983e533cdc5dd3ee116da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba2f26c2e85fe4b06560c888fec471287a4bd62")]
 [assembly: System.Reflection.AssemblyProductAttribute("prototipo2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prototipo2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
